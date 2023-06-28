a72bf4de007410434253c86583174cb58021e62f
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-06-28 14:36:52 -0600

    Update Spalsh.swift

--- MyFlikrApp/Spalsh.swift
+++ MyFlikrApp/Spalsh.swift
@@ -8,17 +8,31 @@
 import SwiftUI
 
 struct Spalsh: View {
+    @State var isActive: Bool = false
+    
     var body: some View {
-        VStack {
-            Text("Flikr Photos")
-                .font(.largeTitle)
-                .bold()
+        ZStack {
+            if self.isActive {
+                MainView()
+            } else {
+                Text("Flikr Photos")
+                    .font(.largeTitle)
+                    .bold()
+            }
                 
         }
         .padding(.horizontal, 88)
         .padding(.vertical, 0)
         .frame(width: 375, height: 812, alignment: .center)
         .background(Color(red: 0.96, green: 0.96, blue: 0.96))
+        
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                withAnimation {
+                    self.isActive.toggle()
+                }
+            }
+        }
         
     }
 }
