0a27ae8bc57acefc71bd32275aaa25042e67ed0e
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-06-28 14:39:10 -0600

    Creating necessary files for my App

---
+++ MyFlikrApp/MainViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  MainViewModel.swift
+//  MyFlikrApp
+//
+//  Created by Guillermo Ruiz Baires on 28/6/23.
+//
+
+import Foundation
+
+
---
+++ MyFlikrApp/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  MyFlikrApp
+//
+//  Created by Guillermo Ruiz Baires on 28/6/23.
+//
+
+import Foundation
