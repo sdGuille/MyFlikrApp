1f68822564e97be4f32e0f9e6a419fda741da0ac
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-06-28 19:35:41 -0600

    Moved to the App folder

---
+++ MyFlikrApp/App/MyFlikrAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFlikrAppApp.swift
+//  MyFlikrApp
+//
+//  Created by Guillermo Ruiz Baires on 28/6/23.
+//
+
+import SwiftUI
+
+@main
+struct MyFlikrAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
