6d48c31dd249354f77eb6ed7b6f1e0c5d67d6a2f
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-06-30 14:43:53 -0600

    Update MainView.swift

--- MyFlikrApp/Views/MainView.swift
+++ MyFlikrApp/Views/MainView.swift
@@ -8,61 +8,36 @@
 import SwiftUI
 
 struct MainView: View {
-    @State private var searchText = ""
+    @StateObject var viewModel = MainViewModel()
+    @State private var hasAppered = false
     
     var body: some View {
-        ZStack {
-            
-            VStack {
-                VStack {
-                    Text("Photos Challenge")
-                        .font(.title3)
-                        .bold()
-                        .multilineTextAlignment(.center)
-                        .foregroundColor(Color(red:0.02, green: 0.02, blue: 0.02))
-                        .padding(.top)
+        NavigationStack {
+            List(viewModel.filteredPhotos) { data in
+                NavigationLink(destination: DetailView(image: data.urlM, title: data.title, description: data.description.content, owner: data.ownername, date: data.datetaken)) {
+                    RowView(image: data.urlM)
+                    //                Text("\(data.ownername)")
                 }
-                .foregroundColor(.clear)
-                .frame(width: 375, height: 88)
-                .background(Color(red: 0.98, green: 0.98, blue: 0.98))
-                .shadow(color: Color(red: 0.65, green: 0.65, blue: 0.67), radius: 0, x: 0, y: 0.33)
-                
-                Spacer()
-                
-                Text("Find Flickr photos by searching names in the search bar below")
-                    .font(.headline)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(.black.opacity(0.6))
-                    .frame(width: 270, height: 81, alignment: .top)
-                Spacer()
-                Spacer()
-                
-                HStack {
-                    Image(systemName: "line.3.horizontal.decrease.circle.fill")
-                        .font(.largeTitle)
-                    //                        .frame(width: 22.57567, height: 21.57863)
-                        .foregroundColor(.black)
-                        .padding(.trailing)
-                    TextField("Search a name", text: $searchText)
-                        .foregroundColor(Color(red: 0.02, green: 0.02, blue: 0.02))
-                        .frame(width: 223.88779, height: 13, alignment: .center)
-                    
-                    
-                }
-                .foregroundColor(.clear)
-                .frame(width: 344, height: 46)
-                .background(Color(red: 0.96, green: 0.96, blue: 0.96))
-                .cornerRadius(5)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 5)
-                        .inset(by: 0.5)
-                        .stroke(.black, lineWidth: 1)
-                )
-                
-                
             }
+            
+            .searchable(text: $viewModel.searchText, prompt: "Search a name")
+            
+            .refreshable {
+                viewModel.handleRefresh()
+            }
+            
+            .task {
+                if hasAppered {
+                    viewModel.loadData()
+                    hasAppered = true
+                }
+            }
+            .navigationTitle("Photo Search")
         }
-        .background(Color(red: 0.96, green: 0.96, blue: 0.96))
+        
+//        .onAppear {
+//            viewModel.fetchData()
+//        }
     }
 }
 
