3a1cc3b79193c2d8c7f5d97d023cf380fa689992
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-06-30 14:42:51 -0600

    This file it's going to be deleted

---
+++ MyFlikrApp/Views/PracticeView.swift
@@ -0,0 +1,28 @@
+//
+//  PracticeView.swift
+//  MyFlikrApp
+//
+//  Created by Guillermo Ruiz Baires on 28/6/23.
+//
+
+//import SwiftUI
+//
+//struct PracticeView: View {
+//    @StateObject var vm = MainViewModel()
+//
+//    var body: some View {
+//        NavigationStack {
+//            List(vm.photosArray) { data in
+//                RowView(image: data.urlM)
+//            }
+//            .navigationTitle("Photo Search")
+//            .searchable(text: $vm.searchText, prompt: "Search a name")
+//        }
+//    }
+//}
+//
+//struct PracticeView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        PracticeView()
+//    }
+//}
