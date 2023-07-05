4890586bd5f05e3802fe29aa8ac31edda28505b4
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-07-05 12:44:28 -0600

    Readme

--- MyFlikrApp/ViewModel/MainViewModel.swift
+++ MyFlikrApp/ViewModel/MainViewModel.swift
@@ -24,7 +24,7 @@ class MainViewModel: ObservableObject {
     }
     
     var urlString: String {
-        return "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=22306363d108b02e9ae093fd1d681092&safe_search=3&extras=url_m%2C+description%2C+date_taken%2C+owner_name&format=json&nojsoncallback=1&auth_token=72157720887275035-a89d64fbfc5d4b2d&api_sig=06dd7cdfc41b9e6ce102d4f85042e4f5"
+        return "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=aaf7230ce699a1dff86eee5a8e068f31&safe_search=3&extras=url_m%2C+description%2C+date_taken%2C+owner_name&format=json&nojsoncallback=1&auth_token=72157720887378078-96a7771aa07bc641&api_sig=c5e96f310a471acb561e94e3cfd0b8af"
     }
 
     init() {
@@ -48,7 +48,6 @@ extension MainViewModel {
             guard let url = URL(string: urlString) else { throw CustomError.invalidURL }
 
             let (data, response) = try await URLSession.shared.data(from: url)
-            let cacheUrl = URLCache.shared.diskCapacity
             guard (response as? HTTPURLResponse)?.statusCode == 200 else { throw CustomError.serverError }
             guard let results = try? JSONDecoder().decode(Photos.self, from: data) else { throw CustomError.invalidData }
             self.photosArray.append(contentsOf: results.photos.photo)
---
+++ MyFlikrApp/ViewModel/README.md
@@ -0,0 +1,12 @@
+#  Flickr Photos
+
+Learned here about:
+1- pagination
+2- Fetch data from an Dictionary with array
+3- Refreshable
+4- DetailView
+5- Learn about how to read the documentation of the Flickr API
+6- Implemented a searchable
+
+/Users/guillermoruizbaires/Desktop/Simulator Screenshot - iPhone 14 Pro - 2023-07-05 at 12.35.56.png
+/Users/guillermoruizbaires/Desktop/Simulator Screenshot - iPhone 14 Pro - 2023-07-05 at 12.36.32.png
