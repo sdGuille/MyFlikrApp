141da913a2a1f6d042789947e44d3819cba9434d
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-11-17 22:30:29 -0600

    Learning about how to parse an API

---
+++ MyFlikrApp/Core/data/api/APIConstants.swift
@@ -0,0 +1,15 @@
+//
+//  APIConstants.swift
+//  MyFlikrApp
+//
+//  Created by Guillermo Ruiz Baires on 10/11/23.
+//
+
+import Foundation
+
+enum APIConstants {
+    static let host = "https://www.flickr.com/services/rest/"
+    static let grantType = "client_credentials"
+    static let clientId = "748446ae0c08e3448197fcd72d33cea5"
+    static let clientSecret = "2303c24031536e67"
+}
